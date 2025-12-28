990711038c71d6fe211e461db094084d531bec18
Ulduz Quliyeva <qlyvaulduz@gmail.com>
2025-12-28 20:41:20 +0400

    task 1

---
+++ src/Task1.java
@@ -0,0 +1,28 @@
+public class Task1 {
+    static void main() {
+        // step 1
+        int age = 12;
+        double height = 1.58;
+        boolean isStudent = true;
+
+        // step 2
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is student: " + isStudent);
+
+        //step 3
+        int points = 85;
+        double pointsDouble = points;
+
+        System.out.println("Points (int): " + points);
+        System.out.println("Points (double): " + pointsDouble);
+
+
+        //step 4  
+        double averageScore = 89.7;
+        int finalScore = (int) averageScore;
+
+        System.out.println("Average score: " + averageScore);
+        System.out.println("Final score: " + finalScore);
+    }
+}
