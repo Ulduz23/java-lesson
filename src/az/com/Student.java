48269b219c35f0e5118e496b15a1f288aafbdd28
Ulduz Quliyeva <qlyvaulduz@gmail.com>
2026-01-11 12:06:12 +0400

    lesson 4

---
+++ src/az/com/Student.java
@@ -0,0 +1,18 @@
+package az.com;
+
+public class Student {
+
+   //lesson 4
+   public String name;
+   public int age;
+   public int id;
+
+   @Override
+   public String toString() {
+      return "Student{" +
+              "name='" + name + '\'' +
+              ", age=" + age +
+              ", id=" + id +
+              '}';
+   }
+}
